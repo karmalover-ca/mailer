7567d73b93ad489867456a1cc1c0aa52b993921b
Liam Pilson <liamphone0@gmail.com>
2024-01-31 05:32:18 -0500

    changes start.sh to reflect new changes

--- example.start.sh
+++ example.start.sh
@@ -2,10 +2,23 @@
 #export IS_PRODUCTION="random"
 export LOG_FILE="latest.log"
 
-export BOT_TOKEN="bot"
-export APPLICATION_ID="id"
-export DEV_SERVER_ID="rd"
+export BOT_TOKEN="botToken"
+export APPLICATION_ID="appId"
+export DEV_SERVER_ID="devServerId"
 
 export VERSION="1.0.0"
+
+export SMTP_HOST="mail.example.com"
+export SMTP_PORT="465"
+export SMTP_USER="example@example.com"
+export SMTP_PASS="password"
+export SMTP_ADDRESS="Placeholder McDoctorate <example@example.com>"
+
+#Channel where messages will get sent
+export CHANNEL_ID="chanId"
+
+#Role to ping in discord message
+export ROLE_ID="roleId"
+
 
 yarn dev
