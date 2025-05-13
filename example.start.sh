d14fdc3d95101ccb259118f16eafed345093d65a
Liam Pilson <Liamphone0@gmail.com>
2025-05-13 18:21:33 +0000

    creates example start.sh

---
+++ example.start.sh
@@ -0,0 +1,28 @@
+# Uncomment in production
+#export IS_PRODUCTION="random"
+#export LOG_FILE="latest.log"
+
+export BOT_TOKEN=""
+export APPLICATION_ID=""
+# Comment in production
+export DEV_SERVER_ID=""
+
+export VERSION="1.0.0"
+
+# SMTP Setup
+export SMTP_HOST="smtp.example.com"
+export SMTP_PORT="465"
+export SMTP_USER="no-reply@example.com"
+export SMTP_PASS=""
+export SMTP_ADDRESS="Example Name <no-reply@example.com>"
+export SMTP_REPLY_TO="example@gmail.com"
+
+#Channel where messages will get sent
+export CHANNEL_ID=""
+export TEST_CHANNEL_ID=""
+
+#Role to ping in discord message
+export ROLE_ID=""
+export TEST_ROLE_ID=""
+
+yarn dev
